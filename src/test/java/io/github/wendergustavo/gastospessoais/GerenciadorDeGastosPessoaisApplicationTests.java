3813c6446a62ee18ea90e63487820a5ad8ba0bb0
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-06 19:49:12 -0300

    Renomeia classe principal para Application

---
+++ src/test/java/io/github/wendergustavo/gastospessoais/GerenciadorDeGastosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wendergustavo.gastospessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeGastosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
