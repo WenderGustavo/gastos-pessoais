e9bedf1e5858849faf3f5d141b8d2eab2fedde1a
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-11 21:59:19 -0300

    test: adicionar testes para a model Usuario

---
+++ src/test/java/io/github/wendergustavo/gastospessoais/Model/UsuarioTest.java
@@ -0,0 +1,11 @@
+package io.github.wendergustavo.gastospessoais.Model;
+
+
+import org.junit.jupiter.api.Test;
+public class UsuarioTest {
+
+    @Test
+    public void testCriarUsuario(){
+
+    }
+}
