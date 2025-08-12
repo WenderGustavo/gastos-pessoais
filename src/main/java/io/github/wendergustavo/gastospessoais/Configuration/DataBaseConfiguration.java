cf6a51a303248f174e8b120ae29875883494ae5a
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-11 22:55:23 -0300

    Criado a configuração do database

---
+++ src/main/java/io/github/wendergustavo/gastospessoais/Configuration/DataBaseConfiguration.java
@@ -0,0 +1,45 @@
+package io.github.wendergustavo.gastospessoais.Configuration;
+
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String user;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver = "org.postgresql.Driver";
+
+
+    @Bean
+    public DataSource hikariDataSource() {
+
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(user);
+        hikariConfig.setPassword(password);
+        hikariConfig.setDriverClassName(driver);
+
+        hikariConfig.setMaximumPoolSize(10); //maximo de conexões liberadas
+        hikariConfig.setMinimumIdle(1); // tamanho inicial do pool
+        hikariConfig.setPoolName("library-db-pool");
+        hikariConfig.setMaxLifetime(600000);// 600 mil ms (10 min)
+        hikariConfig.setConnectionTimeout(100000);// timeout para conseguir conexão
+        hikariConfig.setConnectionTestQuery("select 1");// query de teste
+
+        return new HikariDataSource(hikariConfig);
+
+    }
+}
--- src/test/java/io/github/wendergustavo/gastospessoais/Model/UsuarioTest.java
+++ src/test/java/io/github/wendergustavo/gastospessoais/Model/UsuarioTest.java
@@ -6,6 +6,15 @@ public class UsuarioTest {
 
     @Test
     public void testCriarUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Wender");
+        usuario.setEmail("nem12@gmail.com");
+        usuario.setSenha("nem123");
+
+        assert usuario.getNome().equals("Wender");
+        assert usuario.getEmail().equals("nem12@gmail.com");
+        assert usuario.getSenha().equals("nem123");
 
     }
+
 }
