7dd1892451a8309d0df37c87a86aa24cd72ae223
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-13 19:45:36 -0300

    feat: finalizando a implementação da entendidade Usuario

--- src/main/java/io/github/wendergustavo/gastospessoais/Model/Usuario.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/Model/Usuario.java
@@ -1,10 +1,7 @@
 package io.github.wendergustavo.gastospessoais.Model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -16,10 +13,17 @@ import java.util.UUID;
 public class Usuario {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "nome", length =(100),nullable = false)
     private String nome;
+
+    @Column(name = "email",nullable = false,length =(250), unique = true)
     private String email;
+
+    @Column(name = "senha",length =(250), nullable = false)
     private String senha;
 
 
