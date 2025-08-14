d5745da20388149496d8881e743d5705027030a1
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-13 21:07:12 -0300

    feat: criar entidade Gasto

---
+++ src/main/java/io/github/wendergustavo/gastospessoais/Model/Gasto.java
@@ -0,0 +1,34 @@
+package io.github.wendergustavo.gastospessoais.Model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@RequiredArgsConstructor
+@Data
+public class Gasto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID uuid;
+
+    @Column(name = "descricao", length = (150), nullable = false)
+    private String descricao;
+
+    @Column(name = "valor",precision = 6,scale = 2, nullable = false)
+    private BigDecimal valor;
+
+    @Column(name = "data_gasto", nullable = false)
+    private LocalDate dataGasto;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+}
