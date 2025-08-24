a009a1b47c16e3c582f1b3cd849e7b3c96d4ea08
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-24 18:28:21 -0300

    feat(controller): adiciona UsuarioController

--- src/main/java/io/github/wendergustavo/gastospessoais/Controller/UsuarioController.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/Controller/UsuarioController.java
@@ -1,4 +1,24 @@
 package io.github.wendergustavo.gastospessoais.Controller;
 
+
+import io.github.wendergustavo.gastospessoais.Model.Usuario;
+import io.github.wendergustavo.gastospessoais.Service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public ResponseEntity<Void> salvarUsuario(Usuario usuario){
+        usuarioService.salvar(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+
 }
