a5166cc2484cabfca0a1619eaa3f75f13678f83f
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-24 18:28:59 -0300

    refactor(service): adiciona anotações em UsuarioService

--- src/main/java/io/github/wendergustavo/gastospessoais/Service/UsuarioService.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/Service/UsuarioService.java
@@ -2,10 +2,14 @@ package io.github.wendergustavo.gastospessoais.Service;
 
 import io.github.wendergustavo.gastospessoais.Model.Usuario;
 import io.github.wendergustavo.gastospessoais.Repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UsuarioService {
 
-    private UsuarioRepository usuarioRepository;
+    private final UsuarioRepository usuarioRepository;
 
     public Usuario salvar(Usuario usuario){
         return usuarioRepository.save(usuario);
