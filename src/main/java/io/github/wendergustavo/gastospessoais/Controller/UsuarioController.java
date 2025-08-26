66ed1c7ab4a09d2a62898827d242b49ce7ccc771
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-26 19:34:16 -0300

    refact: adicionando a anotação @requestBody

--- src/main/java/io/github/wendergustavo/gastospessoais/Controller/UsuarioController.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/Controller/UsuarioController.java
@@ -5,8 +5,7 @@ import io.github.wendergustavo.gastospessoais.Model.Usuario;
 import io.github.wendergustavo.gastospessoais.Service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -15,7 +14,8 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
-    public ResponseEntity<Void> salvarUsuario(Usuario usuario){
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
         usuarioService.salvar(usuario);
         return ResponseEntity.ok().build();
     }
