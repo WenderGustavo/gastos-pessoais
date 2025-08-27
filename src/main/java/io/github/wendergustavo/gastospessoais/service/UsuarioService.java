5cc07451f06024ba382a05d4d9f46589a9200153
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-08-26 21:05:54 -0300

    refact: adicionando metodos no UsuarioService

--- src/main/java/io/github/wendergustavo/gastospessoais/service/UsuarioService.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/service/UsuarioService.java
@@ -5,6 +5,9 @@ import io.github.wendergustavo.gastospessoais.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
@@ -14,4 +17,18 @@ public class UsuarioService {
     public Usuario salvar(Usuario usuario){
         return usuarioRepository.save(usuario);
     }
+
+    public Optional<Usuario> buscarPorId(UUID id){
+        return usuarioRepository.findById(id);
+    }
+
+    public void deletar(UUID id){
+        usuarioRepository.deleteById(id);
+    }
+
+    public void atualizar(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+
+
 }
