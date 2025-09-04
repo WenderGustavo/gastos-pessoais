fcba7a92fbddcb8d15ec12d7984d98112ec4a74e
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-09-04 20:51:35 -0300

    feat: criando a classe UsuarioValidator

--- src/main/java/io/github/wendergustavo/gastospessoais/validador/UsuarioValidator.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/validador/UsuarioValidator.java
@@ -1,4 +1,33 @@
 package io.github.wendergustavo.gastospessoais.validador;
 
+
+import io.github.wendergustavo.gastospessoais.model.Usuario;
+import io.github.wendergustavo.gastospessoais.repository.UsuarioRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
 public class UsuarioValidator {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioValidator(UsuarioRepository usuarioRepository) {
+        this.repository = usuarioRepository;
+    }
+
+    public void validarEmail(String email, UUID id){
+
+        Optional<Usuario> usuario = repository.findByEmail(email);
+
+        if(usuario.isPresent()){
+            if (id == null || !id.equals(usuario.get().getId())){
+                throw new RuntimeException("Email ja em uso!");
+            }
+        }
+
+    }
+
+
 }
