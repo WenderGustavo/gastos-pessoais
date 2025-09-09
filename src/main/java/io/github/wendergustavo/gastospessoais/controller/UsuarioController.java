807fb9187ed5a73487ce1d1bed6e87f4c8c6b504
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-09-09 20:09:04 -0300

    refactor: aprimorar métodos do UsuarioController para usar DTOs e mapper

--- src/main/java/io/github/wendergustavo/gastospessoais/controller/UsuarioController.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/controller/UsuarioController.java
@@ -1,12 +1,16 @@
 package io.github.wendergustavo.gastospessoais.controller;
 
 
+import io.github.wendergustavo.gastospessoais.controller.mappers.UsuarioMapper;
+import io.github.wendergustavo.gastospessoais.dto.UsuarioDTO;
 import io.github.wendergustavo.gastospessoais.model.Usuario;
 import io.github.wendergustavo.gastospessoais.service.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -15,29 +19,50 @@ import java.util.UUID;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final UsuarioMapper mapper;
+
 
     @PostMapping
-    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+    public ResponseEntity<Void> salvarUsuario(@RequestBody @Valid UsuarioDTO dto){
+        Usuario usuario = mapper.toEntity(dto);
         usuarioService.salvar(usuario);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> buscarPorId(@PathVariable("id") UUID id){
-        return ResponseEntity.ok(usuarioService.buscarPorId(id).orElseThrow());
+    public ResponseEntity<UsuarioDTO> buscarPorId(@PathVariable("id") UUID id){
+
+        return usuarioService.buscarPorId(id)
+                .map(usuario -> {
+                    UsuarioDTO usuarioResponseDTO = mapper.toDTO(usuario);
+                    return ResponseEntity.ok(usuarioResponseDTO);
+                }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable("id") UUID id){
+
+        Optional<Usuario> usuarioOptional = usuarioService.buscarPorId(id);
+
+        if(usuarioOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
         usuarioService.deletar(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
-    @PutMapping
-    public ResponseEntity<Void> atualizar(@RequestBody Usuario usuario){
-        usuarioService.atualizar(usuario);
-        return ResponseEntity.ok().build();
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable UUID id, @RequestBody @Valid UsuarioDTO dto) {
+        return usuarioService.buscarPorId(id)
+                .map(usuario -> {
+                    usuario.setNome(dto.nome());
+                    usuario.setEmail(dto.email());
+                    usuario.setSenha(dto.senha());
+                    usuarioService.atualizar(usuario);
+                    return ResponseEntity.ok().<Void>build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
 
 }
