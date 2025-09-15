68934bf12c7c75f47dc509ac4c0ada62e1985a23
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-09-15 19:26:16 -0300

    feat: criando a classe GastoService

---
+++ src/main/java/io/github/wendergustavo/gastospessoais/service/GastoService.java
@@ -0,0 +1,20 @@
+package io.github.wendergustavo.gastospessoais.service;
+
+
+import io.github.wendergustavo.gastospessoais.model.Gasto;
+import io.github.wendergustavo.gastospessoais.repository.GastoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GastoService {
+
+    private GastoRepository repository;
+
+    public Gasto salvar(Gasto gasto){
+
+
+        return repository.save(gasto);
+    }
+}
