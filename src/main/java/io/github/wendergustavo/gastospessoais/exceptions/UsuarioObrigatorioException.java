73ab282f77446cbaf34c58674c21bf268705e7ac
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-09-17 18:55:46 -0300

    feat: criacao do enum do gastoTipo

---
+++ src/main/java/io/github/wendergustavo/gastospessoais/exceptions/UsuarioObrigatorioException.java
@@ -0,0 +1,4 @@
+package io.github.wendergustavo.gastospessoais.exceptions;
+
+public class UsuarioObrigatorioException {
+}
---
+++ src/main/java/io/github/wendergustavo/gastospessoais/model/GastoTipo.java
@@ -0,0 +1,18 @@
+package io.github.wendergustavo.gastospessoais.model;
+
+public enum GastoTipo {
+
+    ALIMENTACAO,
+    TRANSPORTE,
+    MORADIA,
+    SAUDE,
+    EDUCACAO,
+    LAZER,
+    VESTUARIO,
+    CONTAS,
+    INVESTIMENTO,
+    IMPOSTOS,
+    SERVICOS,
+    OUTROS
+
+}
