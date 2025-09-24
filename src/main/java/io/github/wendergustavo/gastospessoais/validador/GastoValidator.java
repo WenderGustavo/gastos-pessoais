8d70dcda44234fdbc99b83e9d4a4a61b9c7fde43
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-09-24 18:54:55 -0300

    refactor: atualizar GastoValidator para usar verificações booleanas com tratamento explícito de exceções

--- src/main/java/io/github/wendergustavo/gastospessoais/validador/GastoValidator.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/validador/GastoValidator.java
@@ -1,4 +1,52 @@
 package io.github.wendergustavo.gastospessoais.validador;
 
+import io.github.wendergustavo.gastospessoais.exceptions.CampoInvalidoException;
+import io.github.wendergustavo.gastospessoais.model.Gasto;
+import io.github.wendergustavo.gastospessoais.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
 public class GastoValidator {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public void validarGasto(Gasto gasto) {
+
+        if (!validarValorPositivo(gasto)){
+            throw new CampoInvalidoException("Value must be greater than zero.");
+        }
+
+        if(!validarUsuarioExistente(gasto)){
+            throw new CampoInvalidoException("User not found or not informed.");
+        }
+
+        if (!validarData(gasto)){
+            throw new CampoInvalidoException("Expenditure date cannot be in the future.");
+        }
+
+    }
+
+    private boolean validarValorPositivo(Gasto gasto) {
+        return gasto.getValor() != null && gasto.getValor().compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean validarUsuarioExistente(Gasto gasto) {
+        if (gasto.getUsuario() == null || gasto.getUsuario().getId() == null) {
+            return false;
+        }
+        return usuarioRepository.findById(gasto.getUsuario().getId()).isPresent();
+    }
+
+    private boolean validarData(Gasto gasto) {
+        if (gasto.getDataGasto() == null) {
+            gasto.setDataGasto(LocalDate.now());
+            return true;
+        }
+        return !gasto.getDataGasto().isAfter(LocalDate.now());
+    }
+
+
 }
