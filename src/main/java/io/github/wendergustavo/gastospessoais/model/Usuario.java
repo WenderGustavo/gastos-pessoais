5d17b65c03b1777398b5932df2cae8de61519a2e
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-09-25 20:46:36 -0300

    refactor: ajustar nomes de colunas em Usuario para consistência com banco

--- src/main/java/io/github/wendergustavo/gastospessoais/model/Usuario.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/model/Usuario.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@ToString(exclude = "gastos")
 public class Usuario {
 
     @Id
