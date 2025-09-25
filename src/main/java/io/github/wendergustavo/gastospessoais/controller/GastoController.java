a3b4ecccb997d12d957e6898e0a83385375d6acd
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-09-25 20:44:14 -0300

    feat: iniciar implementação do GastoController

--- src/main/java/io/github/wendergustavo/gastospessoais/controller/GastoController.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/controller/GastoController.java
@@ -1,4 +1,25 @@
 package io.github.wendergustavo.gastospessoais.controller;
 
 public class GastoController {
-}
+
+    private final GastoService gastoService;
+    private final GastoMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<Void> salvarGasto(@RequestBody @Valid Gasto gasto){
+
+        gastoService.salvar(gasto);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResultadoPesquisaDTO> buscarPorID(@PathVariable("id") UUID id){
+
+        return gastoService.buscarPorId(id)
+                .map(usuario -> {
+                    var resultadoPesquisaDTO = mapper.toDTO(usuario);
+                    return ResponseEntity.ok(resultadoPesquisaDTO);
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
