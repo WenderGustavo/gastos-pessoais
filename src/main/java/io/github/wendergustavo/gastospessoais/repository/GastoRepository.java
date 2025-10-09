8d5a2e60e0261f89c9e385640f94a4a0d9953170
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-10-09 19:17:20 -0300

    feat: Criado o metodo existsByUsuario no GastoRepository

--- src/main/java/io/github/wendergustavo/gastospessoais/repository/GastoRepository.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/repository/GastoRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface GastoRepository extends JpaRepository<Gasto, UUID> {
+
+    boolean existsByUsuario(Usuario usuario);
 }
