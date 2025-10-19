3be883bf5b956b92f7e79221a94c9c8a19bb93ee
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-10-19 13:44:29 -0300

    feat: criar DTO ErroResposta para representa as respostas de erro

--- src/main/java/io/github/wendergustavo/gastospessoais/dto/ErroResposta.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/dto/ErroResposta.java
@@ -1,4 +1,20 @@
 package io.github.wendergustavo.gastospessoais.dto;
 
-public record ErroResposta() {
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record ErroResposta(
+        int status,
+        String mensagem,
+        List<ErroCampo> erros)
+{
+    public static ErroResposta respostaPadrao(String mensagem) {
+        return new ErroResposta(HttpStatus.BAD_REQUEST.value(), mensagem, List.of());
+    }
+
+    public static ErroResposta conflito(String mensagem) {
+        return new ErroResposta(HttpStatus.CONFLICT.value(), mensagem, List.of());
+    }
+
 }
