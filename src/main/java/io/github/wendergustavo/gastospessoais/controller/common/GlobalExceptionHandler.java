c3e6312c4b97931217b50adbb1f065858b70881d
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-10-19 13:52:39 -0300

    feat: criar GlobalExceptionHandler para tratamento de exceções nos controllers

--- src/main/java/io/github/wendergustavo/gastospessoais/controller/common/GlobalExceptionHandler.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/controller/common/GlobalExceptionHandler.java
@@ -1,4 +1,59 @@
 package io.github.wendergustavo.gastospessoais.controller.common;
 
+import io.github.wendergustavo.gastospessoais.dto.ErroCampo;
+import io.github.wendergustavo.gastospessoais.dto.ErroResposta;
+import io.github.wendergustavo.gastospessoais.exceptions.CampoInvalidoException;
+import io.github.wendergustavo.gastospessoais.exceptions.OperacaoNaoPermitidaException;
+import io.github.wendergustavo.gastospessoais.exceptions.RegistroDuplicadoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        List<ErroCampo> listasErros = fieldErrors
+                .stream()
+                .map(fe -> new ErroCampo(fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Validation error.",
+                listasErros);
+    }
+
+
+    @ExceptionHandler(RegistroDuplicadoException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErroResposta handleRegistroDuplicadoException(RegistroDuplicadoException e){
+        return ErroResposta.conflito(e.getMessage());
+    }
+
+    @ExceptionHandler(OperacaoNaoPermitidaException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErroResposta handleOperacaoNaoPermitidaException(OperacaoNaoPermitidaException e){
+        return ErroResposta.respostaPadrao(e.getMessage());
+    }
+
+    @ExceptionHandler(CampoInvalidoException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleCampoInvalidoException(CampoInvalidoException e){
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Validation error.",
+                List.of(new ErroCampo(e.getCampo(), e.getMessage())));
+    }
+
+
 }
