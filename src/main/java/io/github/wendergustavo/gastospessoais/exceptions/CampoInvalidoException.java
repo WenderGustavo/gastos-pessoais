d13497280cbcecc0acefaa6d8dd29fd60f2663fa
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-10-19 13:46:38 -0300

    feat: incluir nome do campo em CampoInvalidoException na validação de gasto

--- src/main/java/io/github/wendergustavo/gastospessoais/exceptions/CampoInvalidoException.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/exceptions/CampoInvalidoException.java
@@ -1,4 +1,14 @@
 package io.github.wendergustavo.gastospessoais.exceptions;
 
-public class CampoInvalidoException {
+import lombok.Getter;
+
+public class CampoInvalidoException extends RuntimeException{
+
+    @Getter
+    public String campo;
+
+    public CampoInvalidoException(String campo, String mensagem){
+        super(mensagem);
+        this.campo = campo;
+    }
 }
