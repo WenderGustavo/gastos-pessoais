5fb3d4207e93aecfb61e9c9c374dab65b742498c
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-11-11 20:49:18 -0300

    feat: adicionado o metodo de verificação de valores de gasto no GastoRepository

--- src/main/java/io/github/wendergustavo/gastospessoais/repository/GastoRepository.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/repository/GastoRepository.java
@@ -1,9 +1,12 @@
 package io.github.wendergustavo.gastospessoais.repository;
 
 import io.github.wendergustavo.gastospessoais.entity.Gasto;
+import io.github.wendergustavo.gastospessoais.entity.GastoTipo;
 import io.github.wendergustavo.gastospessoais.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,5 +14,13 @@ public interface GastoRepository extends JpaRepository<Gasto, UUID> {
 
     boolean existsByUsuario(Usuario usuario);
 
+    boolean existsByDescricaoAndGastoTipoAndValorAndDataGastoAndUsuario(
+            String descricao,
+            GastoTipo gastoTipo,
+            BigDecimal valor,
+            LocalDate dataGasto,
+            Usuario usuario
+    );
     List<Gasto> findByUsuarioEmailOrderByDataGastoDesc(String email);
+
 }
