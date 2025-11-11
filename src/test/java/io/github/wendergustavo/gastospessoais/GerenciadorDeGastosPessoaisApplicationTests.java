e8d66c9dd183554eca622778cf1fcda6a3dff225
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-11-10 22:24:37 -0300

    test: corrige classe principal nos testes

--- src/test/java/io/github/wendergustavo/gastospessoais/GerenciadorDeGastosPessoaisApplicationTests.java
+++ src/test/java/io/github/wendergustavo/gastospessoais/GerenciadorDeGastosPessoaisApplicationTests.java
@@ -3,7 +3,7 @@ package io.github.wendergustavo.gastospessoais;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GerenciadorDeGastosPessoaisApplicationTests.class)
 class GerenciadorDeGastosPessoaisApplicationTests {
 
 	@Test
