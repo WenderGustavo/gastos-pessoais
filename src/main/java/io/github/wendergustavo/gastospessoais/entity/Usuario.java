2896c31ac0aec05171761111231e6b40bde2bfeb
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-11-12 20:23:02 -0300

    refact: modificado a coluna role de String para Enum no Usuario

--- src/main/java/io/github/wendergustavo/gastospessoais/entity/Usuario.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/entity/Usuario.java
@@ -31,8 +31,9 @@ public class Usuario {
     @Column(name = "senha",length =(128), nullable = false)
     private String senha;
 
-    @Column(nullable = false, length = 20)
-    private String role;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20,nullable = false )
+    private Roles role;
 
     @OneToMany(mappedBy = "usuario")
     private List<Gasto> gastos;
