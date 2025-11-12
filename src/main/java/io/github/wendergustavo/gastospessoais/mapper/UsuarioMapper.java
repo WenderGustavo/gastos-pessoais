56c3968768ef404a175fee2e05dfed5d1ec6ce0b
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-11-11 21:24:36 -0300

    feat: adicionado o metodo toListResponseDTO no UsuarioMapper para mapeamento da lista de usuarios

--- src/main/java/io/github/wendergustavo/gastospessoais/mapper/UsuarioMapper.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/mapper/UsuarioMapper.java
@@ -1,11 +1,15 @@
 package io.github.wendergustavo.gastospessoais.mapper;
 
 
+import io.github.wendergustavo.gastospessoais.dto.ListaUsuarioResponseDTO;
 import io.github.wendergustavo.gastospessoais.dto.UsuarioDTO;
 import io.github.wendergustavo.gastospessoais.dto.UsuarioResponseDTO;
 import io.github.wendergustavo.gastospessoais.entity.Usuario;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
@@ -13,6 +17,13 @@ public interface UsuarioMapper {
     Usuario toEntity(UsuarioDTO usuarioDTO);
 
     UsuarioResponseDTO toResponseDTO(Usuario usuario);
+
+    default ListaUsuarioResponseDTO toListResponseDTO(List<Usuario> usuarios) {
+        List<UsuarioResponseDTO> dtos = usuarios.stream()
+                .map(this::toResponseDTO)
+                .collect(Collectors.toList());
+        return new ListaUsuarioResponseDTO(dtos);
+    }
 
     void updateEntityFromDTO(UsuarioDTO usuarioDTO,@MappingTarget Usuario usuario);
 }
