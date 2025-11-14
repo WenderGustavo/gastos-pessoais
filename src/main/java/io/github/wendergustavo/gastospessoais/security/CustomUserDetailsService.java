9cbbaea188544ea385f3b895bf4f0a26c5bd87d9
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-11-13 23:59:24 -0300

    feat: criado o CustomUserDetailsService para buscar user no banco e criar o builder

--- src/main/java/io/github/wendergustavo/gastospessoais/security/CustomUserDetailsService.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/security/CustomUserDetailsService.java
@@ -1,4 +1,33 @@
 package io.github.wendergustavo.gastospessoais.security;
 
-public class CustomUserDetailsService {
+import io.github.wendergustavo.gastospessoais.entity.Usuario;
+import io.github.wendergustavo.gastospessoais.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioService.obterPorEmail(email);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário com email " + email + " não encontrado.");
+        }
+
+        return User.builder()
+                .username(usuario.getEmail())
+                .password(usuario.getSenha())
+                .roles(usuario.getRole().name())
+                .build();
+    }
 }
