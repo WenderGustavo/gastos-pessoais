0f855a75a5338f7ee29c739bbe4df57c60e804de
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-11-14 00:00:01 -0300

    criado a config SecurityConfiguration

--- src/main/java/io/github/wendergustavo/gastospessoais/configuration/SecurityConfiguration.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/configuration/SecurityConfiguration.java
@@ -1,4 +1,53 @@
 package io.github.wendergustavo.gastospessoais.configuration;
 
+import io.github.wendergustavo.gastospessoais.security.CustomUserDetailsService;
+import io.github.wendergustavo.gastospessoais.service.UsuarioService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(configurer -> {
+                    configurer.loginPage("/login");
+                })
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login/**").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/usuarios/**").permitAll()
+                            .requestMatchers("/auth/**").permitAll()
+                            .requestMatchers("/usuarios/**").hasRole("ADMIN")
+                            .requestMatchers("/gastos").hasAnyRole("USER","ADMIN");
+
+                    authorize.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    public UserDetailsService  userDetailsService(UsuarioService usuarioService){
+
+        return new CustomUserDetailsService(usuarioService);
+    }
+
+
+
 }
