f2c5c4bc9d8e3d54478ca8603731477bf8b4528b
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-11-13 23:57:45 -0300

    feat: criado a exceção para busca via email UsuarioEmailNaoEncontradoException

--- src/main/java/io/github/wendergustavo/gastospessoais/exceptions/UsuarioEmailNaoEncontradoException.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/exceptions/UsuarioEmailNaoEncontradoException.java
@@ -1,7 +1,7 @@
 package io.github.wendergustavo.gastospessoais.exceptions;
 
 public class UsuarioEmailNaoEncontradoException extends RuntimeException {
-  public UsuarioEmailNaoEncontradoException(String message) {
-    super(message);
+  public UsuarioEmailNaoEncontradoException(String email) {
+    super("Usuário com e-mail " + email + " não encontrado.");
   }
 }
