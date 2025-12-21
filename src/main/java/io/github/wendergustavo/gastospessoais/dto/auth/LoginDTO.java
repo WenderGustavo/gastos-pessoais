23e4a156320f7c1e2a61416d4be29a6283016319
Wender Gustavo de Aquino <nem8723@gmail.com>
2025-12-20 22:32:43 -0300

    chore: atualizar DTO de login

--- src/main/java/io/github/wendergustavo/gastospessoais/dto/auth/LoginDTO.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/dto/auth/LoginDTO.java
@@ -1,6 +1,11 @@
 package io.github.wendergustavo.gastospessoais.dto.auth;
 
+import jakarta.validation.constraints.NotNull;
+
 public record LoginDTO(
+
+        @NotNull(message = "Campo obrigatorio.")
         String email,
+        @NotNull(message = "Campo obrigatorio.")
         String senha
 ) {}
