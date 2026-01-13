f0205b8613fe456f6528453d175c382c8a30aedf
Wender Gustavo de Aquino <nem8723@gmail.com>
2026-01-12 22:40:36 -0300

    feat: criado o metodo de existsByIdAndUsuarioId no GastoRepository

--- src/main/java/io/github/wendergustavo/gastospessoais/repository/GastoRepository.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/repository/GastoRepository.java
@@ -1,7 +1,6 @@
 package io.github.wendergustavo.gastospessoais.repository;
 
 import io.github.wendergustavo.gastospessoais.entity.Gasto;
-import io.github.wendergustavo.gastospessoais.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -10,9 +9,11 @@ import java.util.UUID;
 
 public interface GastoRepository extends JpaRepository<Gasto, UUID>, JpaSpecificationExecutor<Gasto> {
 
-    boolean existsByUsuario(Usuario usuario);
+    boolean existsByUsuarioId(UUID id);
 
     List<Gasto> findByUsuarioEmailOrderByCreatedAtDesc(String email);
 
     List<Gasto> findByUsuarioId(UUID id);
+
+    boolean existsByIdAndUsuarioId(UUID gastoId, UUID usuarioId);
 }
