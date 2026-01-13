753dd002f3395f993cac944b3e7db947a7c682ff
Wender Gustavo de Aquino <nem8723@gmail.com>
2026-01-12 22:41:35 -0300

    feat: criado a classe GastoSecurity para auxilio de verificção de id do usuario

--- src/main/java/io/github/wendergustavo/gastospessoais/security/GastoSecurity.java
+++ src/main/java/io/github/wendergustavo/gastospessoais/security/GastoSecurity.java
@@ -1,4 +1,25 @@
 package io.github.wendergustavo.gastospessoais.security;
 
+import io.github.wendergustavo.gastospessoais.dto.usuario.UsuarioDetailsDTO;
+import io.github.wendergustavo.gastospessoais.repository.GastoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
 public class GastoSecurity {
+
+    private final GastoRepository gastoRepository;
+
+    public boolean isOwner(UUID gastoId) {
+        var auth = (CustomAuthentication) SecurityContextHolder
+                .getContext().getAuthentication();
+
+        UUID usuarioId = ((UsuarioDetailsDTO) auth.getPrincipal()).id();
+
+        return gastoRepository.existsByIdAndUsuarioId(gastoId, usuarioId);
+    }
 }
